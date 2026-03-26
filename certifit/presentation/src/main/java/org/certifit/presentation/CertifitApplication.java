30bc647ffcb0c8f668abec3509f45b7a7004f0c0
FranJosipovic <fran.josipovic55@gmail.com>
2026-03-27 00:24:35 +0100

    configure project to use .env file

--- certifit/presentation/src/main/java/org/certifit/presentation/CertifitApplication.java
+++ certifit/presentation/src/main/java/org/certifit/presentation/CertifitApplication.java
@@ -1,5 +1,6 @@
 package org.certifit.presentation;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CertifitApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(CertifitApplication.class, args);
 
         System.out.println("Hello World");
