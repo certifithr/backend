344db54dd398acffb1bf3ef6930ed1e8044a1684
Svale09 <svalinaivan09+git@gmail.com>
2026-03-29 19:41:05 +0200

    Changed the userService method to return a not found https status instead of just an exception

--- certifit/application/src/main/java/org/certifit/application/user/UserService.java
+++ certifit/application/src/main/java/org/certifit/application/user/UserService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.certifit.db.entity.UserEntity;
 import org.certifit.db.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
@@ -16,6 +18,6 @@ public class UserService {
     public UserEntity getUserById(UUID uuid){
 
         return userRepository.findById(uuid)
-                .orElseThrow(() -> new IllegalArgumentException("No user exist with ID: " + uuid));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user exists with ID: " + uuid));
     }
 }
