57bb8aee0d70934674df6dbed7da86c837a98355
FranJosipovic <fran.josipovic55@gmail.com>
2026-03-29 20:48:37 +0200

    forgot to add Healthcheck to git

--- certifit/presentation/src/main/java/org/certifit/presentation/health/HealthCheckController.java
+++ certifit/presentation/src/main/java/org/certifit/presentation/health/HealthCheckController.java
@@ -1,4 +1,4 @@
-package org.certifit.presentation;
+package org.certifit.presentation.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
