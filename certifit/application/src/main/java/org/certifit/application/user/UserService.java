5173ea69ebc751e8a72f23f99c4e51c1437d60be
Svale09 <svalinaivan09+git@gmail.com>
2026-03-29 19:05:49 +0200

    Created the UserService class

---
+++ certifit/application/src/main/java/org/certifit/application/user/UserService.java
@@ -0,0 +1,14 @@
+package org.certifit.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.certifit.db.entity.UserEntity;
+import org.certifit.db.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
