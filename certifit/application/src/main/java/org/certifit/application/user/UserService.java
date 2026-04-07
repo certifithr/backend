a20ab3d100b8eba09bf7de400cd18edf662d7232
Svale09 <svalinaivan09+git@gmail.com>
2026-04-08 01:11:38 +0200

    Created the GlobalExceptionHandler.java class, the UserNotFoundException.java class and started implementing the logic to remove exception handling from the application layer

--- certifit/application/src/main/java/org/certifit/application/user/UserService.java
+++ certifit/application/src/main/java/org/certifit/application/user/UserService.java
@@ -1,11 +1,10 @@
 package org.certifit.application.user;
 
 import lombok.RequiredArgsConstructor;
+import org.certifit.application.exception.UserNotFoundException;
 import org.certifit.db.entity.UserEntity;
 import org.certifit.db.repository.UserRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
@@ -16,8 +15,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserEntity getUserById(UUID uuid){
-
         return userRepository.findById(uuid)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user exists with ID: " + uuid));
+                .orElseThrow(() -> new UserNotFoundException(uuid));
     }
 }
