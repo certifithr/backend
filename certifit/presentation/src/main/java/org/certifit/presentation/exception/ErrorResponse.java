56c6b448e415672a925e058265201769a4481152
Svale09 <svalinaivan09+git@gmail.com>
2026-04-09 12:24:28 +0200

    Implemented the logic for handling the UserNotFoundException, created an ErrorResponse.java record which is used to build a valid response in case an error is thronw.

---
+++ certifit/presentation/src/main/java/org/certifit/presentation/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package org.certifit.presentation.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String message,
+        String path
+) { }
--- certifit/presentation/src/main/java/org/certifit/presentation/exception/GlobalExceptionHandler.java
+++ certifit/presentation/src/main/java/org/certifit/presentation/exception/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package org.certifit.presentation.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.certifit.application.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({UserNotFoundException.class})
-    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception){
-        return null; //TODO
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException exception, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
